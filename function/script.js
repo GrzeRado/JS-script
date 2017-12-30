5791cf24250fc2aa121a6e7cd85835320c69de8a
GrzeRado <33958248+GrzeRado@users.noreply.github.com>
2017-12-30 06:09:38 +0100

    Add files via upload

---
+++ function/script.js
@@ -0,0 +1,34 @@
+// przyklad funkcji
+
+// function kitt(){
+//}
+
+
+///function kitt(b) {
+   // return b
+//}
+
+
+//var b = "zima"
+  //  function kit(a){
+  //  return b
+//}
+
+//function sum(a, b){
+   // return a+b
+//}
+
+//var x = 2
+///var blabla = function add(x) {
+   // console.log(x)
+   // if (x>0)
+  //  add (--x)
+///}
+
+
+var OurForeach = function (array, otherElement){
+    for (var = i; array.lenght; i++){
+        otherElement(array[i], i, array)
+    }
+
+}
